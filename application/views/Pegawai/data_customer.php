b7ec5f4b7136e879dd22b66786899fe78589d286
imamstts <imam_stts@gmail.com>
2020-09-29 23:47:04 +0700

    tes7

--- application/views/Pegawai/data_customer.php
+++ application/views/Pegawai/data_customer.php
@@ -4,7 +4,7 @@
   <!-- Page Heading -->
   <div class="row">
     <div class="col-8">
-      <h1 class="h3 mb-2 text-gray-800">Master Customer</h1>
+      <h1 class="h3 mb-2 text-gray-800">Master Customerr</h1>
       <p class="mb-4">Edit atau tambah data untuk Customerrr disini</p>
     </div>
     <div class="col-4">

dfe662768563a9af79c8299603869ec67b8d88f3
imamstts <imam_stts@gmail.com>
2020-09-29 23:34:19 +0700

    tes5

--- application/views/Pegawai/data_customer.php
+++ application/views/Pegawai/data_customer.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-8">
       <h1 class="h3 mb-2 text-gray-800">Master Customer</h1>
-      <p class="mb-4">Edit atau tambah data untuk Customer disini</p>
+      <p class="mb-4">Edit atau tambah data untuk Customerr disini</p>
     </div>
     <div class="col-4">
       <?= $this->session->flashdata('pesan'); ?>

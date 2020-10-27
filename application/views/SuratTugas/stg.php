e5eb29c955420629d81f2ec3f31ad5de8bbfd256
imamstts <imam.stts@gmail.com>
2020-10-27 23:35:48 +0700

    revisi

--- application/views/SuratTugas/stg.php
+++ application/views/SuratTugas/stg.php
@@ -50,21 +50,10 @@
 						</table>
 					</div>
 				</div>
-
-				<div id="cetak"></div>
-
-				<!-- <div class="form-group row">
-				  <label class="col-sm-3 col-form-label"></label>
-				  <label class="col-sm-9 col-form-label" >sukses</label>
-				  
-		         
-				</div> -->
 	    	</div>
 				<div class="card-footer">
 	      	<!-- <div class="form-group row"> -->
 				<button type="submit" class="btn btn-success"><i class="fas fa-paper-plane"></i> Kirim</button>
-				<a href="<?= site_url('C_stg/coba')?>"><button type="button" class="btn btn-success" id="cetakkk"><i class="fas fa-paper-plane"></i> tes</button></a>
-				
 			</div>
 			<!-- </div> -->
        
@@ -132,138 +121,12 @@
       $('#mitra_id').val(mitra_id);
       $('#nama_mitra').val(nama_mitra);
       $('#pilihmitra').modal('hide');
-    })
-
-	let daftarPilih = [];
-	//akhir function mitra
-
-	$(document).on('click', '#selectProject', function() {
-	  
-	  let isi = $('#selectProject').val;
-	  console.log(isi);
-
-	  let idproject = $(this).data('id');
-	  daftarPilih.push(idproject)
-	 
-	//   if (idproject ==  20 && cekButton == ) {
-	// 	document.getElementById(idproject + "").innerHTML = "Sudah Terpilih";
-	//   }
-	  
-	//   daftarPilih.forEach(element => {
-	// 	console.log(element);
-	//   });
-	  
-	  
-	  
-	  
-	  
-	 
-	//   if (cekButton == "Pilih") {
-	// 	// document.getElementById("selectProject").innerHTML = "Sudah Terpilih";
-	// 	let id = $(this).data('id');
-	// 	idProject.push(id)
-		
-	//   }
-	//   else{
-	// 	// document.getElementById("selectProject").innerHTML = "Pilih";
-	// 	let id = $(this).data('id');
-	// 	idProject = idProject.filter(item => item !== value)
-
-	//   }
-     
-    })
- 
-	$(document).on('click', '#min', function() {
-      
-	  let today = new Date();
-	  let dd = today.getDate();
-	  let mm = today.getMonth()+1; //January is 0!
-	  let yyyy = today.getFullYear();
-	  if(dd<10){
-			  dd='0'+dd
-		  } 
-		  if(mm<10){
-			  mm='0'+mm
-		  } 
-
-	  today = yyyy+'-'+mm+'-'+dd;
-	  document.getElementById("datefield").setAttribute("min", today);
-
- 	})
-
-
-	$(document).on('click', '#saveProject', function() {
-		$('#pilihproject').modal('hide');
-		console.log(idProject);
-		var text = "";
-		idProject.forEach(element => {
-			
-			var head = `<div class='form-group row'>`;
-			var label = `<label class='col-sm-3 col-form-label'></label>`;
-			var input = "<div class='col-sm-9 input-group-append'>"+ element + 
-			"<button type='button' class='btn btn-info btn-flat'>"
-			+"<i class='far fa-clock'>"+"</i>"+
-		        "</button>"+"</div>";
-			var tutup = "</div>";
-			text +=  head + label + input + tutup;
-			
-
-			// // var x;
-			// for (let i = 1; i <= x; i++) {
-			// 	text += head + label + i + "</label>" + input + "name='sn-" + i+"'" + tutup + "</div>";
-			// }
-			document.getElementById("cetak").innerHTML = text;
-		});
-		
-	})
-
-  })
+    });
+  });
+  //akhir function mitra
 </script>
 <!-- akhir modal mitra -->
 <!-- -------------------------------------------------------------------------------------------------------------------->
-<!-- Modal Project -->
-
-<div class="modal fade" id="pilihproject" tabindex="-2" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Silahkan Pilih project</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="form-group row">
-          <div class="table-responsive">
-            <table class="table table-bordered" width="100%" id="dataTable2" cellspacing="0">
-              <thead>
-              <tr class="text-center">
-                <th>ID Project</th>
-                <th>Pilih</th>
-              </tr>
-            </thead>
-            <tbody>
-			<?php
-              foreach($project->result() as $i => $data)  {?>
-              <tr>
-                <td><?=$data->project_id?></td>
-                <td class="text-center">
-					<button class="btn btn-info" id="select"
-					data-id="<?= $data->project_id?>">Pilih
-					</button>
-                </td>
-              </tr>
-              <?php } ?>
-            </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
 
 <!-- JAVASCRIPT TAIL SELECT -->
 <script src="https://cdnjs.cloudflare.com/ajax//libs//popper.js/1.14.7/umd/popper.min.js"></script>

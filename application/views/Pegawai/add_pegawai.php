7259167ce158f0ea1eea389398cd88ae1102bae9
Olan <rolantwin@gmail.com>
2020-12-14 23:46:30 +0700

    pegawai

--- application/views/Pegawai/add_pegawai.php
+++ application/views/Pegawai/add_pegawai.php
@@ -30,7 +30,7 @@
 				  </div>
 		        </div>
 				<div class="form-group row">
-		          <label class="col-sm-3 col-form-label">No Telepon</label>	
+		          <label class="col-sm-3 col-form-label">Password</label>	
 		          <div class="col-sm-9">
 		            <input type="password" class="form-control" name="password" id="password" required>
 		          </div>

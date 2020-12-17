957e6faefed97951f68029f4edefcc855e60d862
imamstts <imam.stts@gmail.com>
2020-12-17 20:41:09 +0700

    Revisi mitra upload mitra ,

--- application/models/M_LaporanMitra.php
+++ application/models/M_LaporanMitra.php
@@ -85,88 +85,7 @@ class M_LaporanMitra extends CI_Model {
 		// JOIN customer c ON c.customer_id=p.customer_idWHERE h.mitra_id=2;
 	}
 
-    public function add_pdf()
-    {
-		$id = $this->input->post('id');
-		$this->db->select('project_id');
-		$this->db->from('laporan');
-		$this->db->where('project_id', $id);
-		$query = $this->db->get()->row();
-		// pengecekan apakah project_id sudah dibuat atau belum
-    	if ($query == null) {
-    		$data = [
-				"project_id" 		=> $this->input->post('id'),
-				"create_by"			=> $this->session->userdata('mitra_id'),
-				"mitra_id"			=> $this->session->userdata('mitra_id'),
-				"file_pdf"			=> $this->upload->data('file_name'),
-				"create_on"         => date('Y-m-d')
-			];
-			$this->db->insert('laporan', $data);
-		}else{
-			// jika sudah dibuat maka akan melakukan update untuk file map
-			$data = [
-				"file_pdf"			=> $this->upload->data('file_name')
-			];	
-			$id = $this->input->post('id');
-			$this->db->where('project_id', $id);
-			$this->db->update('laporan', $data);
-		}
-		
-	}
-
-    public function add_gdb()
-    {
-		$id = $this->input->post('id');
-		$this->db->select('project_id');
-		$this->db->from('laporan');
-		$this->db->where('project_id', $id);
-		$query = $this->db->get()->row();
-    	if ($query == null) {
-    		$data = [
-				"project_id" 		=> $this->input->post('id'),
-				"create_by"			=> $this->session->userdata('mitra_id'),
-				"mitra_id"			=> $this->session->userdata('mitra_id'),
-				"file_gdb"			=> $this->upload->data('file_name'),
-				"create_on"         => date('Y-m-d')
-    		];
-			$this->db->insert('laporan', $data);
-		}else{
-			// jika sudah dibuat maka akan melakukan update untuk file map
-			$data = [
-				"file_gdb"			=> $this->upload->data('file_name')
-			];
-			$id = $this->input->post('id');
-			$this->db->where('project_id', $id);
-			$this->db->update('laporan', $data);
-		}
-	}
-
-	public function add_bom()
-    {
-		$id = $this->input->post('id');
-		$this->db->select('project_id');
-		$this->db->from('laporan');
-		$this->db->where('project_id', $id);
-		$query = $this->db->get()->row();
-    	if ($query == null) {
-    		$data = [
-				"project_id" 		=> $this->input->post('id'),
-				"create_by"			=> $this->session->userdata('mitra_id'),
-				"mitra_id"			=> $this->session->userdata('mitra_id'),
-				"file_bom"			=> $this->upload->data('file_name'),
-				"create_on"         => date('Y-m-d')
-    		];
-			$this->db->insert('laporan', $data);
-		}else{
-			// jika sudah dibuat maka akan melakukan update untuk file map
-			$data = [
-				"file_bom"			=> $this->upload->data('file_name')
-			];
-			$id = $this->input->post('id');
-			$this->db->where('project_id', $id);
-			$this->db->update('laporan', $data);
-		}
-	}
+    
 	
     public function update_bai()
     {
@@ -192,6 +111,33 @@ class M_LaporanMitra extends CI_Model {
 		$this->db->update('testcom', $data);
 	}
 	//awal function tampilan halaman mitra
+
+	// start Mitra - Menu Survey - upload file
+	public function add_file($pdf,$gdb,$bom)
+	{
+		$id = $this->input->post('id');
+		$this->db->select('project_id');
+		$this->db->from('laporan');
+		$this->db->where('project_id', $id);
+		$query = $this->db->get()->row();
+		// pengecekan apakah project_id sudah dibuat atau belum. jika belumm maka
+    	if ($query == null) {
+    		$data = [
+				"project_id" 		=> $this->input->post('id'),
+				"create_by"			=> $this->session->userdata('mitra_id'),
+				"mitra_id"			=> $this->session->userdata('mitra_id'),
+				"file_pdf"			=> $pdf,
+				"file_gdb"			=> $gdb,
+				"file_bom"			=> $bom,
+				"create_on"         => date('Y-m-d'),
+			
+				
+			];
+			$this->db->insert('laporan', $data);
+		}
+	}
+
+	// end Mitra - Menu Survey - upload file
 	
     
 
